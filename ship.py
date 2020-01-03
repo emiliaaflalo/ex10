90baf2148a93be9ea78d73449021deaaa0da105a
talbrom <talbrom1@gmail.com>
2020-01-03 13:32:46 +0200

    hi

--- ship.py
+++ ship.py
@@ -1,4 +1,4 @@
-
+from screen import Screen
 class Ship:
 
     def __init__(self, x_location, x_speed, y_location, y_speed, prow_tuning):
@@ -7,4 +7,15 @@ class Ship:
         self.y_location = y_location
         self.y_speed = y_speed
         self.prow_tuning = prow_tuning
+
+    def move(self, location, x_speed, y_speed):
+        x, y = location
+        new_x = Screen.SCREEN_MIN_X + (x + x_speed - Screen.SCREEN_MIN_X) % \
+            (Screen.SCREEN_MAX_X - Screen.SCREEN_MIN_X)
+
+    def change_direction(self):
+        if Screen.is_left_pressed():
+            self.prow_tuning += 7
+        elif Screen.is_right_pressed():
+            self.prow_tuning -= 7
 
