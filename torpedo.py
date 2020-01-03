4c6bff6c6776013142bebbbfff700a2f923346af
talbrom <talbrom1@gmail.com>
2020-01-03 13:01:05 +0200

    hi

---
+++ torpedo.py
@@ -0,0 +1,7 @@
+class Torpedo:
+    def __init__(self, x_location, x_speed, y_location, y_speed, direction):
+        self.x_location = x_location
+        self.x_speed = x_speed
+        self.y_location = y_location
+        self.y_speed = y_speed
+        self.direction = direction
