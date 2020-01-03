a89cb91915c8b558e897a92925656928c50efbdd
emiliaaflalo <emilia.aflalo@mail.huji.ac.il>
2020-01-03 13:00:37 +0200

    new line

---
+++ ship.py
@@ -0,0 +1,9 @@
+class Ship:
+
+    def __init__(self, x_location, x_speed, y_location, y_speed, prow_tuning):
+        self.x_location = x_location
+        self.x_speed = x_speed
+        self.y_location = y_location
+        self.y_speed = y_speed
+        self.prow_tuning = prow_tuning
+
