a5f0d81c29cf2492c5d6fdeebcee0b67e91da166
talbrom <talbrom1@gmail.com>
2020-01-03 12:59:08 +0200

    hi

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,8 @@
-print("hello world")
-print("hello emilia")
-print("blah")
-print("hi tal i hate dis")
+
+class Asteroid:
+    def __init__(self, x_location, x_speed, y_location, y_speed, size):
+        self.x_location = x_location
+        self.x_speed = x_speed
+        self.y_location = y_location
+        self.y_speed = y_speed
+        self.size = size
