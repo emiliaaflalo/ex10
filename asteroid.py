19ff20829a57a108ccb35f30e8dbf424f65447f1
emiliaaflalo <emilia.aflalo@mail.huji.ac.il>
2020-01-04 19:45:47 +0200

    new line

--- asteroid.py
+++ asteroid.py
@@ -3,6 +3,39 @@ import math
 
 
 class Asteroid:
+    """
+            A class used to represent an Asteroid
+
+            ...
+
+            Attributes
+            ----------
+            self.x_location : int
+                the x part of coordinates that represent the location of the asteroid
+            self.x_speed : int
+                the x component of the speed of the Asteroid
+            self.y_location : int
+                the y part of coordinates that represent the location of the asteroid
+            self.y_speed : int
+                the y component of the speed of the Asteroid
+            self.size : int
+                the size of the asteroid
+            self.__radius = self.size * 10 - 5
+                the radius of the asteroid calculated according to the size of the asteroid
+
+            Methods
+            -------
+            move():
+                this function updates the location of the asteroid according to the
+                current location and speed
+            has_intersection(obj):
+                this function checks if an object has the same location as an asteroid
+                :param obj: ship or torpedo type object
+                :return: True if there's an intersection, False otherwise
+            get_radius():
+                :return: an int value for the object's radius
+            """
+
     def __init__(self, x_location, x_speed, y_location, y_speed, size):
         self.x_location = x_location
         self.x_speed = x_speed
